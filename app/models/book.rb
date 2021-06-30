bbf8410b048642d7e9b7eee5a3735b1b43cdf9fd
Vincent Yu <vinceyu1228@gmail.com>
2021-06-30 16:13:42 +0800

    add image upload feature

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
+require 'carrierwave/orm/activerecord'
+require 'file_size_validator'
 class Book < ApplicationRecord
+  mount_uploader :image, ImageUploader
+  validates :image, file_size: { maximum: 1.megabytes }
 end
